c58c78e18c88751d33bad22d5b0d0477af224a2e
javadev797982 <javadev797982@gmail.com>
2024-07-17 21:48:05 +0530

    save feature 1.1

---
+++ src/main/java/intellectreversekt/intellectreversekt/IntellectreversektApplication.java
@@ -0,0 +1,13 @@
+package intellectreversekt.intellectreversekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellectreversektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellectreversektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/intellectreversekt/intellectreversekt/IntellectreversektApplicationTests.java
@@ -0,0 +1,13 @@
+package intellectreversekt.intellectreversekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellectreversektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
